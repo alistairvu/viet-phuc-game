b09820a92265cc1a53b67140fe9a2ee390832600
Alistair Vu <alistairvu@gmail.com>
2021-04-04 15:00:26 +0700

    Fix image width

--- components/AppCurrentChoices.tsx
+++ components/AppCurrentChoices.tsx
@@ -1,13 +1,12 @@
 import { choiceSelector } from "../recoil/gameAtom"
 import { useRecoilValue } from "recoil"
-import Image from "next/image"
 
 const AppCurrentChoices: React.FC = () => {
   const choices = useRecoilValue(choiceSelector)
 
   return (
     <div className="mt-lg-0 mt-2 text-center">
-      <Image src={choices} layout="intrinsic" height={600} width={200} />
+      <img src={choices} style={{ maxWidth: 200 }} />
     </div>
   )
 }
